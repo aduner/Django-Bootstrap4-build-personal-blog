4db9b69b49f39bed3ccec4a931aa3283d1c7caf2
aduner <famin854001260@gmail.com>
2020-08-26 23:41:38 +0800

    2020 8.26

--- Django_bolg/urls.py
+++ Django_bolg/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('article/', include('article.urls', namespace='article')),
     path('', home),
+    path('userprofile/', include('userprofile.urls', namespace='userprofile')),
 ]
